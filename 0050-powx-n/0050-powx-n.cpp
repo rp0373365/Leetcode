7eb8e39328fe258ef0f692ceff1bdeffd319d3e7
Rohit@8795 <rp0373365@gmail.com>
2026-08-03 01:35:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.92%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        double ans = 1.0;
+        bool isNegative=false;
+        long long exp = n;
+
+        if (exp < 0) {
+            isNegative = true;
+            exp = -exp;
+        }
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                ans = ans * x;
+                exp = exp - 1;
+            } else {
+                exp = exp / 2;
+                x = x * x;
+            }
+        }
+        if (isNegative) {
+            return (1 / (ans));
+        }
+        return (ans);
     }
 };
