2a7c67044eae3808b352c38137d73d7dbb56cf90
Rohit@8795 <rp0373365@gmail.com>
2026-08-06 22:41:55 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.77%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -16,50 +16,60 @@ public:
         
     }
     int smallestNumber(int n, int t) {
-        int a = product(n+1);
-        if(product(n)%t==0)
+        while(n<1000)
         {
-            return n;
-        }
-        else if(product(n+1)%t==0)
-        {
-            return (n+1);
-        }
-        else if(product(n+2)%t==0)
-        {
-            return (n+2);
-        }
-        else if(product(n+3)%t==0)
-        {
-            return (n+3);
-        }
-        else if(product(n+4)%t==0)
-        {
-            return (n+4);
-        }
-        else if(product(n+5)%t==0)
-        {
-            return (n+5);
-        }else if(product(n+6)%t==0)
-        {
-            return (n+6);
-        }
-        else if(product(n+7)%t==0)
-        {
-            return (n+7);
-        }
-        else if(product(n+8)%t==0)
-        {
-            return (n+8);
-        }
-        else if(product(n+9)%t==0)
-        {
-            return (n+9);
-        }
-        else if(product(n+10)%t==0)
-        {
-            return (n+10);
+            if(product(n)%t==0)
+            {
+                return n;
+            }
+            else{
+                n++;
+            }
         }
         return -1;
+        // if(product(n)%t==0)
+        // {
+        //     return n;
+        // }
+        // else if(product(n+1)%t==0)
+        // {
+        //     return (n+1);
+        // }
+        // else if(product(n+2)%t==0)
+        // {
+        //     return (n+2);
+        // }
+        // else if(product(n+3)%t==0)
+        // {
+        //     return (n+3);
+        // }
+        // else if(product(n+4)%t==0)
+        // {
+        //     return (n+4);
+        // }
+        // else if(product(n+5)%t==0)
+        // {
+        //     return (n+5);
+        // }else if(product(n+6)%t==0)
+        // {
+        //     return (n+6);
+        // }
+        // else if(product(n+7)%t==0)
+        // {
+        //     return (n+7);
+        // }
+        // else if(product(n+8)%t==0)
+        // {
+        //     return (n+8);
+        // }
+        // else if(product(n+9)%t==0)
+        // {
+        //     return (n+9);
+        // }
+        // else if(product(n+10)%t==0)
+        // {
+        //     return (n+10);
+        // }
+        // return -1;
     }
 };
