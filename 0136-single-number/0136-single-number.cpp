e52993ea39b92d245b3008fb00a5a374cc84deff
Rohit@8795 <rp0373365@gmail.com>
2026-08-14 15:39:08 +0530

    Time: 0 ms (100%), Space: 20.7 MB (59.29%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -20,36 +20,39 @@ public:
         // return -1;
 
         // THIS IS BETTER APPROCH:
-        int count = 1;
-        int i = 0;
-        int n = nums.size();
+        // int count = 1;
+        // int i = 0;
+        // int n = nums.size();
 
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
 
-        for (int j = 1; j < n; j++) {
-            if (nums[i] == nums[j]) {
-                count++;
-            } else {
-                if (count == 1) {
-                    return nums[i];
-                }
+        // for (int j = 1; j < n; j++) {
+        //     if (nums[i] == nums[j]) {
+        //         count++;
+        //     } else {
+        //         if (count == 1) {
+        //             return nums[i];
+        //         }
 
-                i = j;
-                count = 1; // count reset
-            }
-        }
-
-        // Last element/group check
-        if (count == 1) {
-            return nums[i];
-        }
-
-        return -1;
-        // int ans=0;
-        // for(int val : nums)
-        // {
-        //     ans = ans^val;
+        //         i = j;
+        //         count = 1; // count reset
+        //     }
         // }
-        // return ans;
+
+        // // Last element/group check
+        // if (count == 1) {
+        //     return nums[i];
+        // }
+
+        // return -1;
+
+        // THIS IS THE MOST OPTIMAL APPROCH 
+
+        int ans=0;
+        for(int val : nums)
+        {
+            ans = ans^val;
+        }
+        return ans;
     }
 };
