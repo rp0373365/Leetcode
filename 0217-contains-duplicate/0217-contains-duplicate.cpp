4bfa0abc95bea43b0a3dc83b3b34a6aee3846b01
Rohit@8795 <rp0373365@gmail.com>
2026-08-18 19:05:35 +0530

    Time: 67 ms (63.36%), Space: 111.2 MB (54.11%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -23,8 +23,11 @@ public:
         int n = nums.size();
         unordered_map<int, int> mpp;
         for (int i = 0; i < n; i++) {
-            ++mpp[nums[i]];
-            if (mpp[nums[i]] > 1) return true;
+            mpp[nums[i]]++;
+            if (mpp[nums[i]] > 1)
+            {
+                return true ;
+            }
         }
         return false;
     }
