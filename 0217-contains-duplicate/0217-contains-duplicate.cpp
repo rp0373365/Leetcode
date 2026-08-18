62f95abf677ea40989898249da8ba13af2f76cbd
Rohit@8795 <rp0373365@gmail.com>
2026-08-18 19:03:11 +0530

    Time: 72 ms (47.61%), Space: 111.4 MB (39.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        //<---------------------------------------->
+        // BRUTE FORCE APPROCH
+        //<---------------------------------------->
+        // int n = nums.size();
+        // unordered_set<int> st;
+        // for(int i = 0 ; i < n ; i++)
+        // {
+        //     st.insert(nums[i]);
+        // }
+        // int m = st.size();
+        // if(n!=m)
+        // {
+        //     return true;
+        // }
+        // return false;
+        //<---------------------------------------->
+        // OPTIMAL APPROCH
+        //<---------------------------------------->
+
         int n = nums.size();
-        unordered_set<int> st;
+        unordered_map<int , int> mpp;
         for(int i = 0 ; i < n ; i++)
         {
-            st.insert(nums[i]);
-        }
-        int m = st.size();
-        if(n!=m)
-        {
-            return true;
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]>1)
+            {
+                return true;
+            }
         }
         return false;
+
     }
 };
