0195b8065092169a5935e213c211813378a3ff77
Rohit@8795 <rp0373365@gmail.com>
2026-08-20 01:06:06 +0530

    Time: 6 ms (6.66%), Space: 42.2 MB (17.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,37 +1,6 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-    //<-------------------------------------------------------------->
-
-    //<-------------------------------------------------------------->
-
-
-
-
-    //    int n = nums.size();
-    //    vector<int>ans(n,1);
-    //    vector<int>suffix(n,1); 
-    //    vector<int>prefix(n,1);  
-
-    //    //prefix
-    //    //prefix[0]=1;
-    //    for(int i = 1; i<n; i++)
-    //    {
-    //     prefix[i]=prefix[i-1]*nums[i-1];
-    //    }
-    //    //suffix
-    //    //suffix[n-1]=1;
-    //    for(int i = n-2; i>=0; i--)
-    //    {
-    //     suffix[i]=suffix[i+1]*nums[i+1];
-    //    }
-    //    //ans
-    //    for(int i = 0; i<n; i++)
-    //    {
-    //     ans[i]=prefix[i]*suffix[i];
-    //    }
-    //    return ans;
-
     int n = nums.size();
     vector<int> prifix(n,1);
     vector<int> suffix(n,1);
@@ -41,10 +10,12 @@ public:
     {
         prifix[i]=nums[i-1]*prifix[i-1];
     }
+    //suffix
     for(int i = n-2 ; i >= 0 ; i--)
     {
         suffix[i]=nums[i+1]*suffix[i+1];
     }
+    //ans
     for(int i = 0; i < n  ; i++)
     {
         ans[i]=prifix[i]*suffix[i];
