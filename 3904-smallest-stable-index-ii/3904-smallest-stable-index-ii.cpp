9a21c56c96042ad4dfaddb0cee087555c70b1042
Rohit@8795 <rp0373365@gmail.com>
2026-09-04 23:48:36 +0530

    Time: 14 ms (42.98%), Space: 210.7 MB (56.88%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -5,13 +5,13 @@ public:
         vector<int> maxi(n) , mini(n);
         maxi[0]=nums[0];
         mini[n-1]=nums[n-1];
-        for(int i = 1 ; i < n ; i++)
-        {
-            maxi[i]=max(maxi[i-1],nums[i]);
-        }
         for(int i = n-2; i>=0;i--)
         {
             mini[i] = min(mini[i+1],nums[i]);
+        }
+        for(int i = 1 ; i < n ; i++)
+        {
+            maxi[i]=max(maxi[i-1],nums[i]);
         }
         for(int i = 0 ; i < n ; i++)
         {
